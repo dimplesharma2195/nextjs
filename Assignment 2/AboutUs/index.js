7de7f2583361d2584f32932157e67246f3e98ded
dimplesharma2195 <77977380+dimplesharma2195@users.noreply.github.com>
2025-05-19 20:17:08 +0530

    index.js

--- Assignment 2/AboutUs/index.js
+++ Assignment 2/AboutUs/index.js
@@ -2,7 +2,7 @@ function AboutUs() {
   return (
     <div>
       <h1>About Us</h1>
-      <p>This is the About Us page of our Next.js application.</p>
+      <p>This is the About Us page.</p>
     </div>
   );
 }
