571d06adbbdb5f3b5de8968479be753cc367b971
dimplesharma2195 <77977380+dimplesharma2195@users.noreply.github.com>
2025-05-19 20:08:31 +0530

    AboutUs.js

---
+++ Assignment 1/AboutUs.js
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page of our Next.js application.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
