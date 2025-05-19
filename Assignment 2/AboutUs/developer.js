4709577c1fc3add1311999cb2bd2d895008ced75
dimplesharma2195 <77977380+dimplesharma2195@users.noreply.github.com>
2025-05-19 20:12:38 +0530

    developer.js

---
+++ Assignment 2/AboutUs/developer.js
@@ -0,0 +1,11 @@
+function Developer() {
+  return (
+    <div>
+      <h1>Developer Information</h1>
+      <p>Developer Name: dimplesharma2195</p>
+      <p>Welcome to my developer page!</p>
+    </div>
+  );
+}
+
+export default Developer;
